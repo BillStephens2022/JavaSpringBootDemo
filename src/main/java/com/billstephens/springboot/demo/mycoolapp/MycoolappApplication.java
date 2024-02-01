dfc2f00d952e1ec31c7607135c3f3d705921b760
BillStephens2022 <two4onebill@yahoo.com>
2024-02-01 14:32:14 -0500

    added more coach classes and added qualifier annotation to specify which coach to use in the route

--- src/main/java/com/billstephens/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/billstephens/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -3,6 +3,14 @@ package com.billstephens.springboot.demo.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// if you were to place a component outside the main folder, you would need
+// to add it to the scanBasePackages.
+//@SpringBootApplication(
+//		scanBasePackages = {"com.billstephens.springboot.demo.mycoolapp",
+//		"com.billstephens.springboot.demo.util"})
+
+
 @SpringBootApplication
 public class MycoolappApplication {
 
--- src/main/java/com/billstephens/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/billstephens/springboot/demo/mycoolapp/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.billstephens.springboot.demo.mycoolapp.rest;
 
 import com.billstephens.springboot.demo.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +12,17 @@ public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
 
-    // define a constructor for dependency injection
+    // Example of constructor injections - define a constructor for dependency injection
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
+
+    // Example of setter injection (instead of contructor injection)
+//    @Autowired
+//    public void setCoach(Coach theCoach) {
+//        myCoach = theCoach;
+//    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
